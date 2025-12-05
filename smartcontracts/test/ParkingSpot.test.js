471a44492e1efad5cf39c2a21d670c2e5df545ca
ogazboiz <ogazboizakpolo@gmail.com>
2025-12-05 16:26:20 +0100

    test: add tests for spot listing functionality

--- smartcontracts/test/ParkingSpot.test.js
+++ smartcontracts/test/ParkingSpot.test.js
@@ -21,6 +21,47 @@ describe("ParkingSpot", function () {
     });
   });
 
-  // Test structure for spot listing, booking, etc. will be added in subsequent commits
+  describe("Spot Listing", function () {
+    it("Should allow listing a new parking spot", async function () {
+      const location = "123 Main St, City";
+      const pricePerHour = ethers.parseEther("1");
+      
+      await expect(parkingSpot.connect(spotOwner).listSpot(location, pricePerHour))
+        .to.emit(parkingSpot, "SpotListed")
+        .withArgs(1, spotOwner.address, location, pricePerHour, anyValue);
+      
+      const spot = await parkingSpot.getSpot(1);
+      expect(spot.id).to.equal(1);
+      expect(spot.owner).to.equal(spotOwner.address);
+      expect(spot.location).to.equal(location);
+      expect(spot.pricePerHour).to.equal(pricePerHour);
+      expect(spot.isAvailable).to.be.true;
+    });
+
+    it("Should revert when listing with zero price", async function () {
+      await expect(
+        parkingSpot.connect(spotOwner).listSpot("Location", 0)
+      ).to.be.revertedWithCustomError(parkingSpot, "InvalidPrice");
+    });
+
+    it("Should increment spot counter", async function () {
+      await parkingSpot.connect(spotOwner).listSpot("Location 1", ethers.parseEther("1"));
+      await parkingSpot.connect(spotOwner).listSpot("Location 2", ethers.parseEther("2"));
+      
+      expect(await parkingSpot.spotCounter()).to.equal(2);
+    });
+
+    it("Should track owner spots", async function () {
+      await parkingSpot.connect(spotOwner).listSpot("Location 1", ethers.parseEther("1"));
+      await parkingSpot.connect(spotOwner).listSpot("Location 2", ethers.parseEther("2"));
+      
+      const ownerSpots = await parkingSpot.getOwnerSpots(spotOwner.address);
+      expect(ownerSpots.length).to.equal(2);
+      expect(ownerSpots[0]).to.equal(1);
+      expect(ownerSpots[1]).to.equal(2);
+    });
+  });
+
+  // More tests will be added in subsequent commits
 });
 
